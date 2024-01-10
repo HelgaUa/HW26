6379a205a32f0fc58bcbe2f230f45b49779e3fd4
HelgaUa <Elgaua@gmail.com>
2024-01-10 18:19:38 +0200

    HW26 completed

--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ import Emoji2 from './images/2.png'
 import Emoji3 from './images/3.png'
 import Emoji4 from './images/4.png'
 import Emoji5 from './images/5.png'
-import {v4 as uuidv4} from 'uuid';
+
+const LOCAL_STORAGE_KEY = 'emj';
 
 const data = [
-    {id: uuidv4(), src: Emoji1, alt: 'Emoji-smile'},
-    {id: uuidv4(), src: Emoji2, alt: 'Emoji-party'},
-    {id: uuidv4(), src: Emoji3, alt: 'Emoji-wink'},
-    {id: uuidv4(), src: Emoji4, alt: 'Emoji-hearts'},
-    {id: uuidv4(), src: Emoji5, alt: 'Emoji-sunglasses'}
+    {id: 'Emoji-smile', src: Emoji1, alt: 'Emoji-smile'},
+    {id: 'Emoji-party', src: Emoji2, alt: 'Emoji-party'},
+    {id: 'Emoji-wink', src: Emoji3, alt: 'Emoji-wink'},
+    {id: 'Emoji-hearts', src: Emoji4, alt: 'Emoji-hearts'},
+    {id: 'Emoji-sunglasses', src: Emoji5, alt: 'Emoji-sunglasses'}
 ];
+
 class App extends Component {
     constructor(props) {
         super(props);
@@ -25,18 +27,24 @@ class App extends Component {
         }
     }
 
+    componentDidMount() {
+        const localStorageData = localStorage.getItem(LOCAL_STORAGE_KEY);
+        if (localStorageData !== null) {
+            console.log(localStorageData);
+            this.setState({
+                emojiCounters: JSON.parse(localStorageData),
+            })
+        }
+    }
+
     onEmojiClick = (id) => {
         this.setState((prevState) => {
             const {emojiCounters} = prevState;
-            console.log('emojiCounters-', emojiCounters);
             const updatedCounters = {...emojiCounters};
-            console.log('updatedCounters-', updatedCounters);
 
-            if (updatedCounters[id] === undefined) {
-                updatedCounters[id] = 1;
-            } else {
-                updatedCounters[id]++;
-            }
+            updatedCounters[id] === undefined ? updatedCounters[id] = 1 : updatedCounters[id]++;
+            localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(updatedCounters));
+
             return {
                 emojiCounters: updatedCounters,
             };
@@ -44,18 +52,35 @@ class App extends Component {
     };
 
     onShowResultsClick = () => {
-console.log('show');
+        const {emojiCounters} = this.state;
+        let winnerVotes = 0;
+        let winnerId;
+        for (const emojiId in emojiCounters) {
+            if (emojiCounters[emojiId] > winnerVotes) {
+                winnerVotes = emojiCounters[emojiId];
+                winnerId = emojiId;
+            }
+        }
+        this.setState({
+            winner: winnerId,
+        })
+    }
 
+    onResetClick = () => {
+        localStorage.removeItem(LOCAL_STORAGE_KEY);
+        this.setState({
+            emojiCounters: {},
+            winner: undefined,
+        });
     }
 
     render() {
         const {emojiCounters, winner} = this.state;
-
         return (
             <div className="App">
                 <h1>Голосування за найкращий смайлик</h1>
-                {data.map((emoji) =>{
-                    return(
+                {data.map((emoji) => {
+                    return (
                         <div key={emoji.id} className='emoji-wrapper'>
                             <Emoji
                                 id={emoji.id}
@@ -75,17 +100,28 @@ console.log('show');
                     name='Show results'
                     onClick={this.onShowResultsClick}
                 />
-                <Button className='btn-outline-danger' name='Reset'/>
 
-                <h2>Результати голосування:</h2>
-                <h3>Переможець:</h3>
-                <div className='winner-img'></div>
-                <p>Кількість голосів:</p>
+                <Button
+                    className='btn-outline-danger'
+                    name='Reset'
+                    onClick={this.onResetClick}
+                />
 
+                <div className='winner-img'>
+                    {winner && (
+                        <div>
+                            <h2>Результати голосування</h2>
+                            <h3>Переможець:</h3>
+                            <img src={data.find((emoji) => emoji.id === winner).src}
+                                 className='winner-emoji'
+                                 alt="Winner-emoji"/>
+                            <p>{`Кількість голосів: ${emojiCounters[winner] || 0}`}</p>
+                        </div>
+                    )}
+                </div>
             </div>
         );
     }
-
 }
 
 export default App;
--- src/components/Button.js
+++ src/components/Button.js
@@ -3,12 +3,14 @@ import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 class Button extends Component {
     render() {
-        const { className, name, onClick }= this.props;
-        return(
-            <button type="button"
-                    className={`btn ${className}`}
-                    onClick={onClick}>
-                    {name}
+        const { className, name, onClick } = this.props;
+        return (
+            <button
+                type="button"
+                className={`btn ${className}`}
+                onClick={onClick}
+            >
+                {name}
 
             </button>
         )
